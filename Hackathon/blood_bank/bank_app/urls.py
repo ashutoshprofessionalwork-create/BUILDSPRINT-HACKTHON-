7facb824683c2801135582d65b0974ec59eee2ce
Professional-Sattwik <100934946+SVintities@users.noreply.github.com>
2026-08-29 23:36:24 +0530

    rough idea of pages and their addresses

--- Hackathon/blood_bank/bank_app/urls.py
+++ Hackathon/blood_bank/bank_app/urls.py
@@ -5,4 +5,13 @@ urlpatterns = [
     path('',views.index,name='home-page'),
     path('user-registration/',views.user_registration,name='user-registration'),
     path('donor-registration/',views.donor_registration,name='donor-registration'),
+    path('user-login/',views.user_login,name='user-login'),
+    path('donor-login/',views.donor_login,name='donor-login'),
+    path('aboutUS/',views.aboutUS,name='aboutUS'),
+    path('donor-info/',views.donor_info,name='donor-info'),
+    path('user-info/',views.user_info,name='user-info'),
+    path('donor-registration/',views.donor_registration,name='donor-registration'),
+    path('donor-registration/',views.donor_registration,name='donor-registration'),
+    path('donor-registration/',views.donor_registration,name='donor-registration'),
+    path('donor-registration/',views.donor_registration,name='donor-registration'),
 ]
